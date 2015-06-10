89905d00ff805bb10342dfd4a233ba74d54e6d28
Humphrey Chen <hsinlovehsin@gmail.com>
2015-06-09 23:47:39 -0500

    Add MaximalSquare.cpp to test git function

---
+++ MaximalSquare.cpp
@@ -0,0 +1,32 @@
+// MaximalSquare.cpp : Defines the entry point for the console application.
+/*
+Given a 2D binary matrix filled with 0's and 1's, find the largest square containing all 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+
+Return 4.
+*/
+
+#include "stdafx.h"
+#include "iostream"
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        
+    }
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
